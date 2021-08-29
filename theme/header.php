0de3dc93e83b18209920ef3c237301d6b4bf88b4
JoseTarinT <joseluis_tt88@hotmail.com>
2021-08-29 17:15:50 +1000

    New changes

--- theme/header.php
+++ theme/header.php
@@ -3,8 +3,8 @@
     <head>
         <meta charset="<?php bloginfo('charset'); ?>">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <!-- <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.css" />
-        <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" /> -->
+        <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.css" />
+        <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
         <?php wp_head(); ?>
     </head>
     <body <?php body_class(); ?>>
